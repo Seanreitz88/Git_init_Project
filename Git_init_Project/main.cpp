59432aef666a614daa1a64ecc2f2479a6f7376d3
seanonmission <kyo463@hotmail.com>
2021-08-23 21:17:34 +0800

    Initial Commit

---
+++ Git_init_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Git_init_Project
+//
+//  Created by Yuansheng on 8/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
